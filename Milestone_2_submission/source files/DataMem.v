3c8db61c233a0743c5b7908502a991ca4d0181d9
Mohamed Ragab <98807470+MohamedRagabAbbas@users.noreply.github.com>
2024-03-28 21:17:24 +0200

    add 30 instructions..

---
+++ Milestone_2_submission/source files/DataMem.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/05/2024 09:09:13 AM
+// Design Name: 
+// Module Name: DataMem
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module DataMem (
+    input clk, 
+    input MemRead, 
+    input MemWrite,
+    input [5:0] addr, 
+    input [31:0] data_in, 
+    output [31:0] data_out
+    );
+    
+    initial begin
+        mem[0]= 32'd10;
+        mem[1]= 32'd5;
+//        mem[2]=32'd25;
+//        mem[3]=32'd30;
+//        mem[4]=32'd12;
+    end
+
+    reg [31:0] mem [0:63];
+    always @(posedge clk) begin
+        if(MemWrite == 1)
+            mem[addr] <= data_in; 
+    end
+    assign data_out = (MemRead == 1 ? mem[addr] : 0);
+endmodule 
---
+++ Milestone_2_submission/source files/RegFile.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+/*******************************************************************
+*
+* Module: RegFile.v
+* Project: Milestone 2
+* Author: Mohamed Abbas mohamed_abbas02@aucegypt 
+          Omar Bahgat omar_bahgat@aucegypt.edu
+* Description: Register File 
+*
+* Change history: 
+**********************************************************************/
+
+
+module RegFile #(parameter N = 32)(
+    input [4:0] src1,
+    input [4:0] src2,
+    input [4:0] dest,
+    input [N-1:0] data,
+    input clk,
+    input rst,
+    input RegWrite,
+    output [N-1:0] out1,
+    output [N-1:0] out2
+    );
+    
+    reg [N-1:0] registers [N-1:0];
+    integer i;
+    always @(posedge clk) begin
+        if(rst == 1'b1) begin
+            for(i = 0; i < N; i = i + 1) begin
+                registers[i] <= 0;
+            end
+        end
+        else begin
+            if(RegWrite == 1'b1 && dest != 0) begin
+                registers[dest] <= data;
+            end
+        end
+    end
+    assign out1 = registers[src1];
+    assign out2 = registers[src2];
+endmodule
